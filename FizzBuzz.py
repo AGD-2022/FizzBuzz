fe66ba22f5c0ccdc04922ad71700acd2a2d972bd
AGD-2022 <117851653+AGD-2022@users.noreply.github.com>
2023-05-09 10:08:58 +0200

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,9 +1,13 @@
-for number in range(1, 101):
-    if number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz")
-    elif number % 3 == 0:
-        print("Fizz")
-    elif number % 5 == 0:
-        print("Buzz")
-    else:
-        print(number)
+def fizz_buzz(max_number):    
+
+    for number in range(1, max_number + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+          
+fizz_buzz(100)
